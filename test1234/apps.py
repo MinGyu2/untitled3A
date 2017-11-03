1bbf9923ed21fab35eaf3dc375221e4e17b70c2a
MinGyu2 <31990118+MinGyu2@users.noreply.github.com>
2017-11-04 04:50:47 +0900

    Initial commit

---
+++ test1234/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class Test1234Config(AppConfig):
+    name = 'test1234'
---
+++ test1234/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def post_list(request):
+    return render(request,'test1234/post_list.html');
