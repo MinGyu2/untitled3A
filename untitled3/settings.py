5dd3ae7aeb55e415b96761d5dd62286a848b8375
MinGyu2 <31990118+MinGyu2@users.noreply.github.com>
2017-11-05 00:40:25 +0900

    223eqweqw

--- untitled3/settings.py
+++ untitled3/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret!f
 SECRET_KEY = 'nuvdcxbt#9!kf-e57t!8bj8zma*3hh3z=^w7l=uoomblt7rifs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
