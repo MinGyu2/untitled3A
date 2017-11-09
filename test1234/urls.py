2f301d2ad7560225870ce19ff50ee1585dd5115f
MinGyu2 <31990118+MinGyu2@users.noreply.github.com>
2017-11-09 17:19:02 +0900

    223eqweqwasdasdggg

--- test1234/urls.py
+++ test1234/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^aaaa/$.*',views.test_list,name='test_list'),
 ]
--- test1234/views.py
+++ test1234/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request,'test1234/post_list.html');
+
+def test_list(request):
+    return render(request,'test1234/testliii.html');
